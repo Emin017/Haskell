ae114c9ba52b18ff7e0bfddb53c70a6ac83f489e
Qiming Chu <cchuqiming@gmail.com>
2024-11-28 22:27:51 +0800

    [hs] Add solutions for 99 questions
    * Problem 1
    * Problem 2
    * Problem 3

---
+++ src/list/p1.hs
@@ -0,0 +1,15 @@
+module Main where
+
+main :: IO ()
+main = do
+    print "Output the last element of a list"
+    print (myLast [1, 2, 3, 4, 5])
+    print "Output the last but one element of a list"
+    print (myLast ['x', 'y', 'z'])
+
+-- Find the last element of a list.
+myLast :: [a] -> a
+myLast [] = error "Empty list"
+myLast [x] = x
+-- Recursive call to myLast function to get the last element of the list
+myLast (_:xs) = myLast xs
---
+++ src/list/p2.hs
@@ -0,0 +1,15 @@
+module Main where
+
+main :: IO ()
+main = do
+    print "Output the last-but-one element of a list"
+    print (myButLast [1, 2, 3, 4, 5])
+    print "Output the last-but-one element of a list"
+    print (myButLast ['a'..'z'])
+
+-- Find the last-but-one (or second-last) element of a list.
+myButLast :: [a] -> a
+myButLast [] = error "Empty list"
+myButLast [x] = error "List has only one element"
+myButLast [x, _] = x
+myButLast (_:xs) = myButLast xs
