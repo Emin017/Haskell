84a648b380e785005043ab956d5289c85c2adfa7
Qiming Chu <cchuqiming@gmail.com>
2024-11-28 22:37:36 +0800

    [hs] Add p4 and p5 solutions for 99 questions

---
+++ src/list/p4.hs
@@ -0,0 +1,13 @@
+module Main where
+
+main :: IO ()
+main = do
+    print "Output the number of elements in a list"
+    print (myLength [123, 456, 789])
+    print "Output the number of elements in a list"
+    print (myLength "Hello, world!")
+
+-- Find the number of elements in a list.
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
